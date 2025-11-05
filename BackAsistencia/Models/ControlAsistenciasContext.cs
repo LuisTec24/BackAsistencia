7fe70712d5f5f807abe9b17940543f5ead096746
MishiiKnight <156273760+MishiiKnight@users.noreply.github.com>
2025-11-04 21:02:46 -0600

    Cambio en conApp / Ya toma el appsettings del ControlAsistenciasContext

--- BackAsistencia/Models/ControlAsistenciasContext.cs
+++ BackAsistencia/Models/ControlAsistenciasContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using BackAsistencia.Models;
 
 namespace BackAsistencia.Models;
 
@@ -39,9 +40,8 @@ public partial class ControlAsistenciasContext : DbContext
 
     public virtual DbSet<Salon> Salons { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LUIS\\SQLEXPRESS; DataBase=ControlAsistencias;Integrated Security=true; TrustServerCertificate=True");
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    => optionsBuilder.UseSqlServer("Server=LUIS\\SQLEXPRESS; DataBase=ControlAsistencias;Integrated Security=true; TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
