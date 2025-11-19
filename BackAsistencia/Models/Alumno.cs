97c6add5e44f040efc3b190f2be506fefb63be01
MishiiKnight <156273760+MishiiKnight@users.noreply.github.com>
2025-11-19 11:30:58 -0600

    AsistenciaProfe ready

--- BackAsistencia/Models/Alumno.cs
+++ BackAsistencia/Models/Alumno.cs
@@ -6,15 +6,15 @@ namespace BackAsistencia.Models;
 
 public partial class Alumno
 {
-    public string NumeroControl { get; set; }
+        public string NumeroControl { get; set; }
 
-    public string Nombre { get; set; } = null!;
+        public string Nombre { get; set; } = null!;
 
-    public string Carrera { get; set; } = null!;
+        public string Carrera { get; set; } = null!;
 
-    public string Semestre { get; set; } = null!;
+        public string Semestre { get; set; } = null!;
 
-    public string Contrasena { get; set; } = null!;
+        public string Contrasena { get; set; } = null!;
 
     public virtual ICollection<Asistencia> Asistencia { get; set; } = new List<Asistencia>();
 
